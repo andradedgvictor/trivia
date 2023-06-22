0b035530c8029b996d353cc7e622c68981aeebc3
Lucas Alves <alvesdebrito.lucas@gmail.com>
2023-06-22 18:34:42 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import './App.css';
+import Game from './pages/Game';
 
 class App extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/game" component={ Game } />
       </Switch>
     );
   }
