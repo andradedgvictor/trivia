c5ff24917e17b2b233d72a723ba601b180b367d3
Lucas Alves <alvesdebrito.lucas@gmail.com>
2023-06-22 16:14:17 -0300

    feat: cria a pasta redux

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+import playerReducer from './player';
+
+const rootReducer = combineReducers({ playerReducer });
+
+export default rootReducer;
---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const middlewares = applyMiddleware(thunk);
+
+const store = createStore(rootReducer, composeWithDevTools(middlewares));
+
+if (window.Cypress) {
+  window.store = store;
+}
+
+export default store;
